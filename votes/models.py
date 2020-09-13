9d7f9884dfde6745ca496577de8675732b7ac45b
amsuredev <amsure00dev@gmail.com>
2020-09-13 15:31:59 +0300

    Change field author in Vote

--- votes/models.py
+++ votes/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 # Create your models here.
+from django.contrib.auth.models import User
 
 
 #it is better to limit
 class Vote(models.Model):
-    id_user = models.PositiveIntegerField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     id_pizza = models.PositiveIntegerField()
     vote_mark = models.PositiveIntegerField()
