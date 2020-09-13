2a03f5a5a7d74afe3cf3776b7408324e6ac9046a
amsuredev <amsure00dev@gmail.com>
2020-09-13 16:00:23 +0300

    implement tests of votes app

--- votes/models.py
+++ votes/models.py
@@ -1,10 +1,15 @@
 from django.db import models
 # Create your models here.
 from django.contrib.auth.models import User
+from pizzas.models import Pizza
 
 
 #it is better to limit
 class Vote(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    id_pizza = models.PositiveIntegerField()
+    pizza = models.ForeignKey(Pizza, on_delete=models.CASCADE)
     vote_mark = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.id
+
--- votes/tests.py
+++ votes/tests.py
@@ -1,3 +1,33 @@
 from django.test import TestCase
-
+from django.contrib.auth.models import User
 # Create your tests here.
+from .models import Vote
+from pizzas.models import Pizza
+
+
+class VoteTests(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        #create test user
+        test_user1 = User.objects.create(
+            username='test_user1',
+            password="test_user1_password"
+        )
+        test_user1.save()
+        #create test_pizza
+        test_pizza1 = Pizza.objects.create(
+            name="test_pizza1",
+            price="7"
+        )
+        test_pizza1.save()
+        #create vote
+        test_vote = Vote.objects.create(author=test_user1, pizza=test_pizza1, vote_mark=4)
+        test_vote.save()
+
+    def test_vote_content(self):
+        vote = Vote.objects.get(id=1)
+        self.assertEqual(f'{vote.author}', 'test_user1')
+        self.assertEqual(f'{vote.pizza}', 'test_pizza1')
+        self.assertEqual(vote.vote_mark, 4)
+
+
