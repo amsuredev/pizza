d85f52110672a8676b04d17c66c56e3922b862f6
amsuredev <amsure00dev@gmail.com>
2020-09-15 21:38:52 +0300

    add authentication via api

--- api/urls.py
+++ api/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, include
 from .library import get_res_of_voting
 from .views import PizzaList, PizzaDetail, VoteList, VoteDetail, UserList
 
@@ -7,6 +7,7 @@ urlpatterns = [
     path('pizzas/<int:pk>/', PizzaDetail.as_view()),
     path('votes/', VoteList.as_view()),
     path('votes/<int:pk>', VoteDetail.as_view()),
-    path('voting_res', get_res_of_voting),
-    path('users', UserList.as_view())
+    path('voting_res/', get_res_of_voting),
+    path('users/', UserList.as_view()),
+    path('auth/', include('dj_rest_auth.urls'))#/auth/login/
 ]
--- pizza_project/settings.py
+++ pizza_project/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'rest_framework.authtoken',
+    'dj_rest_auth',
 
     'pizzas.apps.PizzasConfig',
     'votes.apps.VotesConfig',
@@ -127,5 +129,9 @@ STATIC_URL = '/static/'
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.IsAuthenticatedOrReadOnly',
+    ],
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.SessionAuthentication',
+        'rest_framework.authentication.TokenAuthentication'
     ]
 }
